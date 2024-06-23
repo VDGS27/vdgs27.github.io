8e292e1264005190356969871a193b994044ef4b
vadim vasilachi <foiton456@gmail.com>
2024-06-23 16:26:00 +0300

    uppdate app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
 
 
-let tg = window.Telegram.WebApp
-tg.expand()
+let tg = window.Telegram.WebApp;
+tg.expand();
 
 const allRadioButtons = [...document.querySelectorAll("input")];
 allRadioButtons.forEach((item) => {
-    item.checked = false
-})
+    item.checked = false;
+});
 
 const sendButton = document.querySelector(".send-button");
 
 const answers = allRadioButtons.map(x => x.name)
     .filter((value, index, array) => array.indexOf(value) === index);
 
-const greetings = document.querySelector(".greetings")
-greetings.innerText = `Здравствуйте, ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`
+const greetings = document.querySelector(".greetings");
+greetings.innerText = `Здравствуйте, ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 const getCheckedRadioButtonsByName = (name) => {
     let checkedRadioButton = allRadioButtons.filter(x => x.name === name && x.checked);
     if (checkedRadioButton.length === 0){
-        return null
+        return null;
     }
     return  checkedRadioButton[0];
 }
 
 const GetAnswerIndex = (radioId) => {
-    let radioIdSplit = radioId.split("_")
+    let radioIdSplit = radioId.split("_");
     return radioIdSplit[radioIdSplit.length - 1];
 }
 
@@ -37,13 +37,14 @@ const sendQuestions = () => {
         const answer = answers[i];
         const checkedRadioButton = getCheckedRadioButtonsByName(answer);
         if (checkedRadioButton === null){
-            alert("Пожалуйста, ответьте на все вопросы !!!")
+            alert("Пожалуйста, ответьте на все вопросы !!!");
             return;
         }
         questions[answer] = GetAnswerIndex(checkedRadioButton.id);
     }
 
-    tg.sendData(questions)
+    tg.sendData(questions);
+    tg.close();
 }
 
 sendButton.addEventListener("click", sendQuestions);
