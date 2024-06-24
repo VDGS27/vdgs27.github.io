ab08fc0e6f8e6274768c0ba191cf0f1eee78c89a
vadim vasilachi <foiton456@gmail.com>
2024-06-24 14:45:29 +0300

    uppdate app.js and index.html

--- js/app.js
+++ js/app.js
@@ -11,15 +11,9 @@ allRadioButtons.forEach((item) => {
     item.checked = false;
 });
 
-// console.log(tg.initData)
-
-const sendButton = document.querySelector(".send-button");
 
 const answers = allRadioButtons.map(x => x.name)
     .filter((value, index, array) => array.indexOf(value) === index);
-
-// const greetings = document.querySelector(".greetings");
-// greetings.innerText = `Здравствуйте, ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 const getCheckedRadioButtonsByName = (name) => {
     let checkedRadioButton = allRadioButtons.filter(x => x.name === name && x.checked);
@@ -49,12 +43,8 @@ const sendQuestions = () => {
         questions[answer] = GetAnswerIndex(checkedRadioButton.id);
     }
 
-    window.Telegram.WebApp.sendData("1");
-    Telegram.WebApp.close();
+    tg.sendData(JSON.stringify(questions));
+    tg.close();
 }
 
-sendButton.addEventListener("click", sendQuestions);
-Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    window.Telegram.WebApp.sendData("1");
-    tg.close();
-});
+tg.onEvent("mainButtonClicked", sendQuestions);
